100088c630e165f2c416191b2379a0a38620cd6a
AnimeshKotka <animeshk@capitalnumbers.com>
2022-09-23 15:59:02 +0530

    deployement done by animesh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Personal-Portfolio-Angular';
+  title = "Animesh's Portfoli";
 }
